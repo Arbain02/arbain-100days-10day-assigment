c1151840d91bd8638d9c59c39ca73dfb8c4e93e1
Arbain02 <147217625+Arbain02@users.noreply.github.com>
2023-10-19 20:30:55 +0700

    Add files via upload

---
+++ Day11Arbain menghitung luas persegi panjang.java
@@ -0,0 +1,20 @@
+
+package day11.arbain;
+import java.util.Scanner;
+public class Day11Arbain {
+
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+     System.out.println("Masukkan panjang persegi panjang:");
+     double panjang = scanner.nextDouble();
+     System.out.println("Masukkan lebar persegi panjang:");
+     double lebar = scanner.nextDouble();
+     //Area = panjang*lebar;
+     double area = panjang*lebar;
+     System.out.println("Luas persegi panjang adalah:"+area);
+ 
+
+    }
+    
+}
